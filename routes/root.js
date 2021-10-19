b9585b0786153c0fb224e8dd4887c7c6d9c8eb2b
Jake Swackhammer <j.100101@gmail.com>
2021-10-19 19:52:59 -0400

    added dummy endpoints

--- routes/root.js
+++ routes/root.js
@@ -2,8 +2,10 @@ import express from 'express'
 
 let router = express.Router()
 
+router.use(express.static('../public'))
+
 router.get('/', (req, res)=>{
-    res.send('Home Page HTML Goes Here')
+    res.sendFile('../public/index.html')
 })
 router.get('/notes', (req, res)=>{
     res.send('Notes HTML Goes Here')
