fc2ec3ef5db1033f9848ac7f1a9498f896b02146
Jake Swackhammer <j.100101@gmail.com>
2021-10-20 18:16:37 -0400

    added TODOs, impletemenet api get

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import root from './routes/root.js'
 import api from './routes/api.js'
+import morgan from 'morgan';
 
 const app = express()
 //TODO: Deploy to Heroku
 const port = process.env.PORT || 3001
 
+morgan('tiny')
 app.use(express.static('./public', {index: false}))
 app.use('/', root)
 app.use('/api', api)
